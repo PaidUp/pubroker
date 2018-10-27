752ffefa69cfd26476dc02f6b9b89ab4c1fccf2b
Ricardo Lara <riclara@gmail.com>
2018-10-27 18:11:47 -0500

    hot fix signup bulk parent accounts

--- server/service/user.service.js
+++ server/service/user.service.js
@@ -3,14 +3,14 @@ import trae from '@/util/trae'
 import randomstring from 'randomstring'
 
 export default class UserService {
-  static signup ({ firstName, lastName, email, phone, type = 'customer', password = randomstring.generate(12) }) {
-    return trae(`${config.api.user}`, 'POST', { type, password, firstName, lastName, email, phone })
+  static signup ({ firstName, lastName, email, emailSuggested, phone, type = 'customer', password = randomstring.generate(12) }) {
+    return trae(`${config.api.user}`, 'POST', { type, password, firstName, lastName, email, emailSuggested, phone })
       .then(userResponse => userResponse)
       .catch(errors => errors)
   }
 
-  static fbSignup ({ accessToken, phone, rememberMe = false }) {
-    return trae(`${config.api.user}/signup/fb`, 'POST', { accessToken, phone, rememberMe })
+  static fbSignup ({ accessToken, phone, emailSuggested, rememberMe = false }) {
+    return trae(`${config.api.user}/signup/fb`, 'POST', { accessToken, phone, rememberMe, emailSuggested })
       .then(userResponse => userResponse)
       .catch(errors => errors)
   }

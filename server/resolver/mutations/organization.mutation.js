c4f787ba031ba52d9578c8cb7d2ccec96ac23098
Ricardo Lara <riclara@gmail.com>
2019-01-31 09:36:28 -0500

    remove log

--- server/resolver/mutations/organization.mutation.js
+++ server/resolver/mutations/organization.mutation.js
@@ -3,7 +3,6 @@ import { Roles, validate } from '@/util/requireRole'
 
 export default {
   createPaymentPlan: validate([Roles.CHAP, Roles.API])(async (parent, args) => {
-    console.log('arg: ', args)
     const response = await OrganizationService.createPaymentPlan(args)
     return response
   })
